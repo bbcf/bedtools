ed605ab1e74ca37b62e2ed27bd3abcc6f7272721
Jacques Rougemont <jacques.rougemont@epfl.ch>
2012-04-27 13:04:43 +0200

    chromosomes and fields done

--- src/utils/bedFile/sqlFile.cpp
+++ src/utils/bedFile/sqlFile.cpp
@@ -1,40 +1,65 @@
-#include "sqlFile.h"
+#include "bedFile.h"
 
 void SqlFile::Open(void) {
-    // TODO: handle error
+    std::string sql_exec;
     sqlite3_open_v2(bedFile.c_str(), &_db, SQLITE_OPEN_READONLY, NULL);
-    /****
-         TODO: read chromosome list, switch to next when done with one, etc.
-         TODO: variable field set, blablabla
-    ****/
-    std::string sql_exec("select start, end, name, score from 'chr1' order by start,end,name"); 
+    sql_exec = std::string("select name,length from 'chrNames'"); 
     int rc = sqlite3_prepare_v2(_db, sql_exec.c_str(), -1, &_stmt, 0);
-    _prev_chrom = std::string("chr1");
+    _chrom_table.clear();
+    while (sqlite3_step(_stmt) == SQLITE_ROW) {
+        _chrom_table[std::string((char*)sqlite3_column_text(_stmt, 0))] = (long)sqlite3_column_int(_stmt, 1);
+    }
+    I_chrom = _chrom_table.begin();
+    prepareNextChrom(I_chrom->first);
 }
 
 void SqlFile::Close(void) {
+    _chrom_table.clear();
     sqlite3_finalize(_stmt);
     sqlite3_close(_db);
 }
 
+inline void SqlFile::prepareNextChrom( const std::string &chrom ) {
+    _prev_chrom = chrom;
+    int rc = sqlite3_finalize(_stmt);
+    std::string sql_exec = std::string("select * from '")+_prev_chrom+std::string("' order by start, end, name"); 
+    rc = sqlite3_prepare_v2(_db, sql_exec.c_str(), -1, &_stmt, 0);
+}
+
 bool SqlFile::GetNextBed(BED &bed, bool forceSorted) {
     int rc = sqlite3_step(_stmt);
+    int ncol;
+    std::string sql_exec;
     _status = BED_INVALID;
     switch( rc ) {
     case SQLITE_DONE:
-        /*** TODO: finalize, then start next chrom ***/
-        // int rc = sqlite3_reset(_stmt);
-        // _prev_chrom = "chr?";
-        //return GetNextBed(bed, forceSorted);
-        _status = BED_BLANK;
-        return false;
+        I_chrom++;
+        if (I_chrom == _chrom_table.end()) {
+            _status = BED_BLANK;
+            return false;
+        }
+        prepareNextChrom(I_chrom->first);
+        return GetNextBed(bed, forceSorted);
+        break;
     case SQLITE_ROW:
-        bed.chrom = std::string("chr1");
+        bed.chrom = _prev_chrom;
         bed.start = (CHRPOS)sqlite3_column_int(_stmt, 0);
         bed.end = (CHRPOS)sqlite3_column_int(_stmt, 1);
-        bed.name = std::string((char*)sqlite3_column_text(_stmt, 2));
-        bed.score = std::string((char*)sqlite3_column_text(_stmt, 3));
-//        bed.strand = std::string(sqlite3_column_int(_stmt, 4)>0 ? "+" : "-");
+        bed.fields.clear();
+        ncol = 2;
+        while (1) {
+            const char *_name = sqlite3_column_name(_stmt, ncol);
+            if (_name == NULL) break;
+            if (std::string("name").compare(_name) == 0) 
+                bed.name = std::string((char*)sqlite3_column_text(_stmt, ncol));
+            else if (std::string("score").compare(_name) == 0) 
+                bed.score = std::string((char*)sqlite3_column_text(_stmt, ncol));
+            else if (std::string("strand").compare(_name) == 0) 
+                bed.strand = std::string(sqlite3_column_int(_stmt, ncol)>0 ? "+" : "-");
+            else 
+                bed.fields.push_back(std::string((char*)sqlite3_column_text(_stmt, ncol)));
+            ncol++;
+        }
         _prev_start = bed.start;
         _status = BED_VALID;
         return true;
--- src/utils/bedFile/sqlFile.h
+++ src/utils/bedFile/sqlFile.h
@@ -2,7 +2,6 @@
 #define SQLFILE_H
 
 #include <sqlite3.h> 
-#include "bedFile.h"
 
 class SqlFile : public BedFile {
 
@@ -12,17 +11,26 @@ public:
     void Close(void);
     template <typename T> inline void SqlReportBed(const T &bed, 
                                                    CHRPOS start, CHRPOS end, const char* finalize) {
-        /****   TODO: fields business ****/
         if (start<0) start = bed.start;
         if (end<0) end = bed.end;
-        printf ("%s\t%d\t%d\t%s\t%s%s", 
-                bed.chrom.c_str(), start, end, bed.name.c_str(),
-                bed.score.c_str(), finalize);
+        printf ("%s\t%d\t%d\t%s", 
+                bed.chrom.c_str(), start, end, bed.name.c_str());
+        if (bed.score.size()) printf ("\t%s", bed.score.c_str());
+        if (bed.strand.size()) printf ("\t%s", bed.score.c_str());
+        for ( std::vector<std::string>::const_iterator othIt = bed.fields.begin();
+              othIt != bed.fields.end(); othIt++ ) {
+            printf("%s\t", othIt->c_str());
+        }
+        printf ("%s", finalize);
     }
 
 private: 
     sqlite3 *_db;
     sqlite3_stmt *_stmt;
+    std::map< std::string, long > _chrom_table;
+    std::map< std::string, long >::const_iterator I_chrom;
+
+    inline void prepareNextChrom( const std::string& );
 
 };
 
